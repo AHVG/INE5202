7c84ca946257aa9398dd281d8343d4e99a3229ab
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2024-03-14 13:44:22 -0300

    first commit

---
+++ exercicio_1.py
@@ -0,0 +1,19 @@
+
+def exp(x):
+    """
+    Calcula e^x ≅ 1/0! + x¹/1! + x²/2! + x³/3! + x⁴/4!
+    """
+    acc_x = acc_fat = result = 1.0
+
+    for i in range(1, 5):
+        acc_x *= x
+        acc_fat *= i
+        result += acc_x / acc_fat
+
+    return result
+
+def main():
+    x = 2
+    print(f"Resultado e^{x}:", exp(x))
+
+main()
