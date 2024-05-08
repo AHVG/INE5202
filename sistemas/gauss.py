c3b7effa676ec32fa74e062c8811015954157d8a
Augusto H. V. Guerner <augusto.h.v.guerner@gmail.com>
2024-05-07 21:48:48 -0300

    handling conflicts

---
+++ sistemas/gauss.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+def gauss(A, B):
+
+    print(A)
+    print(B)
+    print()
+
+    # Usar 4 algarismos significativos?
+    for column in range(A.shape[1] - 1):
+        for line in range(column + 1, A.shape[1]):
+            factor = - A[line, column] / A[column, column]
+            A[line, :] = A[line, :] + factor * A[column, :]
+            B[line, :] = B[line, :] + factor * B[column, :]
+            print(f"Factor {factor}")
+            print(f"A {A}")
+            print(f"B {B}")
+            print()
+
+    limit = np.finfo(A.dtype).eps
+    A[np.abs(A) < limit] = 0.0
+    limit = np.finfo(B.dtype).eps
+    B[np.abs(B) < limit] = 0.0
+
+    X = np.array(B[A.shape[1] - 1, :] / A[A.shape[1] - 1, A.shape[1] - 1])
+
+    for line in range(A.shape[1] - 2, -1, -1):
+        s = np.sum(np.array([A[line, A.shape[0] - i - 1] * value for i, value in enumerate(X)]))
+        x = (B[line, :] - s) / A[line, A.shape[0] - X.shape[0] - 1]
+        X = np.append(X, x)
+
+    print(f"A {A}")
+    print(f"X {X}")
+    print(f"B {B}")
+
+    return None
+
+def main():
+    A = np.array([
+        [-0.421, 0.784, 0.279],
+        [0.448, 0.832, 0.193],
+        [0.421, 0.784, -0.207]
+    ])
+    B = np.array([[0.0],
+                  [1.0],
+                  [0.0]])
+
+    result = gauss(A, B)
+    print(f"result {result}")
+
+if __name__ == "__main__":
+    main()
